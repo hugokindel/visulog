8ea1d29db326831ede0e532f48836e5aad5d30d3
KINDEL Hugo <hugokindel.pro@protonmail.com>
2020-09-25 12:11:48 +0200

    Document all main folder's files of the analyzer project.

--- analyzer/src/main/java/up/visulog/analyzer/Analyzer.java
+++ analyzer/src/main/java/up/visulog/analyzer/Analyzer.java
@@ -2,21 +2,38 @@ package up.visulog.analyzer;
 
 import up.visulog.config.Configuration;
 import up.visulog.config.PluginConfig;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+/**
+* This is the main analyzer class.
+* It provides a wrapper to run various plugins (a task).
+* based on a configuration (see the config project) and return a result.
+*/
 public class Analyzer {
+	/** The configuration to use in order to know which plugins to compute. */
     private final Configuration config;
 
+    /** The result obtain at the end of the computation of all plugins. */
     private AnalyzerResult result;
 
+    /**
+    * Class constructor.
+    *
+    * @param config The configuration containing the list of plugins to compute.
+    */
     public Analyzer(Configuration config) {
         this.config = config;
     }
 
+    /**
+    * Compute the results of all plugins based on the configuration given
+    * when creating this class.
+    *
+    * @return a result containing the result of all plugins.
+    */
     public AnalyzerResult computeResults() {
         List<AnalyzerPlugin> plugins = new ArrayList<>();
         for (var pluginConfigEntry: config.getPluginConfigs().entrySet()) {
@@ -33,8 +50,15 @@ public class Analyzer {
         return new AnalyzerResult(plugins.stream().map(AnalyzerPlugin::getResult).collect(Collectors.toList()));
     }
 
-    // TODO: find a way so that the list of plugins is not hardcoded in this factory
+    /**
+    * Create an instance of a plugin.
+    *
+    * @param pluginName The name of the plugin.
+    * @param pluginConfig The config to use when creating the plugin.
+    * @return a plugin only if it exists.
+    */
     private Optional<AnalyzerPlugin> makePlugin(String pluginName, PluginConfig pluginConfig) {
+    	// TODO: find a way so that the list of plugins is not hardcoded in this factory
         switch (pluginName) {
             case "countCommits" : return Optional.of(new CountCommitsPerAuthorPlugin(config));
             default : return Optional.empty();
--- analyzer/src/main/java/up/visulog/analyzer/AnalyzerResult.java
+++ analyzer/src/main/java/up/visulog/analyzer/AnalyzerResult.java
@@ -2,23 +2,33 @@ package up.visulog.analyzer;
 
 import java.util.List;
 
+/** This is a wrapper for various plugin's results. */
 public class AnalyzerResult {
+    /** The list of each plugin's result individually. */
+    private final List<AnalyzerPlugin.Result> subResults;
+
+    /** @return a list of the result of each plugin's individually. */
     public List<AnalyzerPlugin.Result> getSubResults() {
         return subResults;
     }
 
-    private final List<AnalyzerPlugin.Result> subResults;
-
+    /**
+    * Class constructor.
+    *
+    * @param subResults The list of results to keep.
+    */
     public AnalyzerResult(List<AnalyzerPlugin.Result> subResults) {
         this.subResults = subResults;
     }
 
+    /** Returns a string representation of the object. */
     @Override
     public String toString() {
         return subResults.stream().map(AnalyzerPlugin.Result::getResultAsString).reduce("", (acc, cur) -> acc + "\n" + cur);
     }
 
+    /** Returns an HTML div representation of the object. */
     public String toHTML() {
-        return "<html><body>"+subResults.stream().map(AnalyzerPlugin.Result::getResultAsHtmlDiv).reduce("", (acc, cur) -> acc + cur) + "</body></html>";
+        return "<html><body>" + subResults.stream().map(AnalyzerPlugin.Result::getResultAsHtmlDiv).reduce("", (acc, cur) -> acc + cur) + "</body></html>";
     }
 }
