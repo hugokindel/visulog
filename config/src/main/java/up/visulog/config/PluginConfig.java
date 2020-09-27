ef36112fcf9d3a6eec330ec327149035b560cfde
CLIPAL moise <elymo.mc@gmail.com>
2020-09-27 14:09:44 +0200

    Document all classes inside config project.

--- config/src/main/java/up/visulog/config/PluginConfig.java
+++ config/src/main/java/up/visulog/config/PluginConfig.java
@@ -1,5 +1,10 @@
 package up.visulog.config;
 
 // TODO: define what this type should be (probably a Map: settingKey -> settingValue)
+
+/**
+ * Describe the plugins used by the git command.
+ * Such as plugin name and plugin value.
+ */
 public interface PluginConfig {
 }
--- config/src/test/java/up/visulog/config/TestConfiguration.java
+++ config/src/test/java/up/visulog/config/TestConfiguration.java
@@ -2,7 +2,7 @@ package up.visulog.config;
 
 public class TestConfiguration {
     /*
-    Do not forget writing tests as soon as class Configuration contains more than just getters
+    TODO: Do not forget writing tests as soon as class Configuration contains more than just getters
     (or if they or the constructor start doing something smart!).
      */
 }
