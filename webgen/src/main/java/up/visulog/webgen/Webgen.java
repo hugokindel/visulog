106b98d5371d71bb1cfe63d16d1a222890f5c121
Jylan Payet <jylanpayet@gmail.com>
2020-11-02 15:58:55 +0100

    Uptade of the HTML in the HTMLEntities and Webgene.

--- webgen/src/main/java/up/visulog/webgen/Webgen.java
+++ webgen/src/main/java/up/visulog/webgen/Webgen.java
@@ -25,9 +25,12 @@ public class Webgen {
       try {
          PrintWriter p = new PrintWriter("output/results-" + new SimpleDateFormat("yyyy_MM_dd-HH-mm").format(new Date()) + ".html");
          p.write(HTMLEntities.DOCTYPE + HTMLEntities.HEAD);
-         p.write("<body>\n<div class=\"results\">\n");
+         p.write("<body>\n" + HTMLEntities.HEADER+ "\n<div class=\"results\">\n");
+         p.write("<div class=\"pluginTextual\">");
          p.write(result.getSubResults().stream().map(AnalyzerPlugin.Result::getResultAsHtmlDiv).reduce("", (acc, cur) -> acc + cur));
-         p.write("</div>\n</body>\n</html>");
+         p.write("\n</div>\n <div class=\"pluginGraphical\">");
+         //todo: Add js librairy to creat the graph
+         p.write("</div></div>\n</body>\n</html>");
          p.close();
       } catch (FileNotFoundException e) {
          e.printStackTrace();
