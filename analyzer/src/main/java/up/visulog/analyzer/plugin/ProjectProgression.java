bf8f8adfb066606b11d234bd8b734c9ddc2bd220
CLIPAL moise <elymo.mc@gmail.com>
2020-11-20 20:03:57 +0100

    code progress for the plugin

--- analyzer/src/main/java/up/visulog/analyzer/plugin/ProjectProgression.java
+++ analyzer/src/main/java/up/visulog/analyzer/plugin/ProjectProgression.java
@@ -3,20 +3,46 @@ package up.visulog.analyzer.plugin;
 import up.visulog.analyzer.AnalyzerPlugin;
 import up.visulog.analyzer.AnalyzerShape;
 import up.visulog.analyzer.ChartTypes;
+import up.visulog.config.Configuration;
+import up.visulog.gitrawdata.Commit;
 
+import java.util.List;
 import java.util.Map;
 
 public class ProjectProgression implements AnalyzerPlugin {
 
+    private final Configuration configuration;
+
+    private Result result;
+
+    public ProjectProgression(Configuration generalConfiguration) {
+        this.configuration = generalConfiguration;
+    }
+    static Result processLog(List<Commit> gitLog) {
+        var result = new Result();
+
+
+        for (var commit : gitLog) {
+
+            var nb = result.resultsMap.getOrDefault(commit.date.toString(), 0);
+            result.resultsMap.put(commit.date.toString(), nb + 1);
+        }
+
+
+
+        return result;
+    }
+
     @Override
     public void run() {
+        this.result = processLog(Commit.parseLogFromCommand(configuration.getGitPath()));
     }
 
     @Override
-    public AnalyzerPlugin.Result getResult() {
-        return null;
+    public Result getResult() {
+        if (result == null) run();
+        return result;
     }
-
     static class Result extends AnalyzerShape implements AnalyzerPlugin.Result{
 
 
@@ -25,9 +51,7 @@ public class ProjectProgression implements AnalyzerPlugin {
         }
 
         @Override
-        public String getResultAsString() {
-            return null;
-        }
+        public String getResultAsString() { return this.resultsMap.toString(); }
 
         @Override
         public String getResultAsHtmlDiv() {
@@ -36,18 +60,20 @@ public class ProjectProgression implements AnalyzerPlugin {
 
         @Override
         public Map<String, Integer> getResults() {
-            return null;
+            return this.resultsMap;
         }
 
         @Override
         public String getPluginName() {
-            return null;
+            return this.pluginName;
         }
 
         @Override
         public String getChartType() {
-            return null;
+            return this.chartType.type;
         }
+
+
     }
 
 }
