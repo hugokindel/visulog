5817badf18faa1043bc7be63618d55c37fada585
CLIPAL moise <elymo.mc@gmail.com>
2020-11-26 12:54:37 +0100

    code progress for Branch.java

--- gitrawdata/src/main/java/up/visulog/gitrawdata/Branch.java
+++ gitrawdata/src/main/java/up/visulog/gitrawdata/Branch.java
@@ -1,5 +1,20 @@
 package up.visulog.gitrawdata;
 
+import java.util.List;
+
 public class Branch {
-    // TODO:Make structure
+    public final String id;
+    public final String date;
+    public final List<String> author;
+    public final List<Commit> commit;
+
+
+    public Branch(String id, String date, List<String> author, List<Commit> commit ){
+        this.id = id;
+        this.date = date;
+        this.author = author;
+        this.commit = commit;
+    }
+
+
 }
