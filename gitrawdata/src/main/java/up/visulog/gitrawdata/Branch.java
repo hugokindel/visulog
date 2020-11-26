652bf306e82e407607bcfb1a137f690bbc3b5696
Jylan Payet <jylanpayet@gmail.com>
2020-11-26 23:26:38 +0100

    Implement Branch in gitrawdata.

--- gitrawdata/src/main/java/up/visulog/gitrawdata/Branch.java
+++ gitrawdata/src/main/java/up/visulog/gitrawdata/Branch.java
@@ -1,20 +1,33 @@
 package up.visulog.gitrawdata;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.ListBranchCommand;
+import org.eclipse.jgit.lib.Ref;
+import java.io.File;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
-
 public class Branch {
     public final String id;
-    public final String date;
-    public final List<String> author;
-    public final List<Commit> commit;
+    public final String name;
 
-
-    public Branch(String id, String date, List<String> author, List<Commit> commit ){
+    public Branch(String id, String name ){
         this.id = id;
-        this.date = date;
-        this.author = author;
-        this.commit = commit;
+        this.name=name;
+
     }
-
-
+    public static List<Branch> parseAllBranchFromRepository(Path gitPath) {
+        try {
+            Git git = Git.open(new File(gitPath.toAbsolutePath().toString())) ;
+            List<Ref> call = git.branchList().setListMode(ListBranchCommand.ListMode.REMOTE).call();
+            List<Branch> branch = new ArrayList<>();
+            for (Ref ref : call) {
+                branch.add(revBranchToBranch(ref));
+            }
+            return branch;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
