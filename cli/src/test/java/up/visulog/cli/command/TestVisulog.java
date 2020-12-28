33edf4b5fad303605a1be8cf4c898dba4fcbe615
JAUROYON maxime <maxime.jauroyon@gmail.com>
2020-12-28 14:12:06 +0100

    Fix CI

--- cli/src/test/java/up/visulog/cli/command/TestVisulog.java
+++ cli/src/test/java/up/visulog/cli/command/TestVisulog.java
@@ -9,7 +9,7 @@ public class TestVisulog {
     @Test
     public void testArgumentParser() {
         Visulog visulog = new Visulog();
-        visulog.run(new String[] {"..", "--plugins=CountCommitsPerAuthor"});
+        visulog.run(new String[] {"..", "--plugins=CountCommitsPerAuthor", "--n-open"});
         assertEquals("..", visulog.getValue());
         assertEquals("CountCommitsPerAuthor", visulog.plugins[0]);
     }
@@ -18,7 +18,7 @@ public class TestVisulog {
     @Test
     public void testProgramWithRealArguments() {
         Visulog visulog = new Visulog();
-        visulog.run(new String[] {"..", "--plugins=CountCommitsPerAuthor,CountLinesPerAuthor"});
+        visulog.run(new String[] {"..", "--plugins=CountCommitsPerAuthor,CountLinesPerAuthor", "--n-open"});
         assertEquals("CountCommitsPerAuthor", visulog.plugins[0]);
         assertEquals("CountLinesPerAuthor", visulog.plugins[1]);
 
